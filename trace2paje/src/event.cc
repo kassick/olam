104b9332c3a261b8db9bc13f837479bce3f02227
Rodrigo Kassick <kassick@gmail.coM>
2011-09-01 16:03:05 -0300

    Example Input

---
+++ trace2paje/src/event.cc
@@ -0,0 +1,102 @@
+// C++ source code
+// File: "/home/kassick/Work/olam/trace2paje/src/event.cc"
+// Created: "Sex, 02 Set 2011 15:23:14 -0300 (kassick)"
+// Updated: "Sex, 02 Set 2011 18:09:54 -0300 (kassick)"
+// $Id$
+// Copyright (C) 2011, Rodrigo Virote Kassick <rvkassick@inf.ufrgs.br> 
+/*
+ * ===========================================================================
+ *
+ *       Filename:  event.cc
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  02-09-2011 15:23:14 BRT
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:   (), 
+ *        Company:  
+ *
+ * ==========================================================================
+ */
+
+
+#include "paje.hh"
+#include "event.hh"
+#include "symbols.hh"
+#include <sstream>
+#include <string>
+#include <map>
+#include <iostream>
+#include <rastro.h>
+
+using namespace std;
+
+bool Paje::Event::trigger(event_id_t evt_id, double timestamp,
+          symbols_table_t * symbols, ostream &out)
+{
+  out << "Abstract class, nothing to see here" << endl;
+  return false;
+}
+
+
+bool Paje::StateEvent::trigger(event_id_t evt_id, double timestamp,
+    symbols_table_t * symbols, ostream &out)
+{
+  string containerName, eventValue;
+  
+  containerName = format_values(eventType->container->formatName, symbols);
+
+
+  if (evt_id == start_id) {
+    eventValue = format_values(this->formatValue, symbols);
+
+    pajePushState(timestamp,
+                  containerName,
+                  eventType->typeName,
+                  eventValue, out);
+
+  } else {
+    pajePopState(timestamp,
+        containerName,
+        eventType->typeName,
+        out);
+  }
+
+  return true;
+}
+
+bool Paje::Event::load_symbols(rst_event_t *event, symbols_table_t * symbols)
+{
+  int count_c, count_w, count_i, count_l, count_f, count_d, count_s;
+  Paje::Symbol *symbol;
+
+
+  Paje::identifier_list_t::iterator it;
+  for (it = identifier_names.begin(); it != identifier_names.end(); ++it)
+  {
+    string &field_name = (*it).field_name;
+    switch ((*it).type) {
+      CASE_TYPE( c , uint8);
+      CASE_TYPE( w , uint16);
+      CASE_TYPE( i , uint32);
+      CASE_TYPE( l , uint64);
+      CASE_TYPE( f , float);
+      CASE_TYPE( d , double);
+      CASE_TYPE( s , string);
+
+      default:
+        cerr << "Unknown type !?!?!? " <<endl;
+        return false;
+        break;
+    }
+
+    (*symbols)[field_name] = symbol;
+
+  }
+
+  (*symbols)["EVT_NAME"]->set_value(this->name.c_str());
+
+}
--- trace2paje/src/event.hh
+++ trace2paje/src/event.hh
@@ -1,7 +1,7 @@
 // C++ source code
 // File: "/home/kassick/Work/olam/trace2paje/src/event.hh"
 // Created: "Qua, 03 Ago 2011 16:14:50 -0300 (kassick)"
-// Updated: "Qua, 03 Ago 2011 16:17:38 -0300 (kassick)"
+// Updated: "Sex, 02 Set 2011 18:05:52 -0300 (kassick)"
 // $Id$
 // Copyright (C) 2011, Rodrigo Virote Kassick <rvkassick@inf.ufrgs.br> 
 /*
@@ -22,21 +22,86 @@
  * ===========================================================================
  */
 
+
+#ifndef __EVENT_H__
+#define __EVENT_H__
+
 #include "paje.hh"
+#include "event.hh"
+#include "symbols.hh"
+#include "container.hh"
 #include <sstream>
 #include <string>
+#include <map>
+#include <list>
+#include <iostream>
+#include <rastro.h>
 
 namespace Paje {
+
+  typedef unsigned int event_id_t;
+
+  typedef struct _identifier_entry_t {
+    string field_name;
+    Paje::basic_types_t type;
+  } identifier_entry_t;
+
+  typedef list<identifier_entry_t> identifier_list_t;
+
   class EventType {
     public:
       string typeName;
-      Container * cont;
+      Container * container;
   };
 
   class Event {
     public:
       EventType *eventType;
 
+      string formatValue;
+      string name;
+
+      identifier_list_t identifier_names;
+
+      bool trigger(event_id_t evt_id, double timestamp,
+          symbols_table_t * symbols, ostream &out);
+
+  
+     bool load_symbols(rst_event_t *event, symbols_table_t * symbols);
+
   };
+
+
+  class StateEvent : Event {
+    public:
+      event_id_t start_id, end_id;
+
+      StateEvent(event_id_t _start_id, event_id_t _end_id): start_id{_start_id}, end_id{_end_id} {};
+
+      bool trigger(event_id_t evt_id, double timestamp,
+          symbols_table_t * symbols, ostream &out);
+  } ;
 }
 
+typedef std::map<std::string      , Paje::Event *>      event_name_map_t;
+typedef std::map<Paje::event_id_t , Paje::Event *>      event_id_map_t;
+typedef std::map<std::string      , Paje::EventType *>  event_type_name_map_t;
+
+
+
+#define CASE_TYPE(SHORT_NAME , PAJE_NAME) \
+        case SHORT_NAME:                                                             \
+          if (count_ ## SHORT_NAME < event->ct.n_ ## PAJE_NAME) {              \
+            symbol = new Paje::Symbol();                                         \
+            symbol->set_value( event->v_ ## PAJE_NAME [ count_ ## SHORT_NAME ] );\
+            count_ ## SHORT_NAME ++  ;                                      \
+          } else {                                                          \
+            cerr << "Not enought values of type " << #SHORT_NAME  <<endl;   \
+            return false;                                                   \
+          }                                                                 \
+          break; // trick?
+
+
+
+
+#endif
